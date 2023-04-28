b3f17c9b4dc0e7184f9968cb6a1cda9e756a9640
JeanGautierSherpas <jean@sherpas.com>
2023-04-28 17:13:33 +0200

    book every friday at 1am

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,13 +1,13 @@
 const functions = require("firebase-functions");
-import { addDays, format, startOfWeek } from "date-fns";
+import { format } from "date-fns";
 const postManRequest = require("postman-request");
 
 const DINARD_BOOK_API =
   "https://dinard.reservations-golf.fr/depart-enregistrement.php";
 
-exports.book = functions.https.onRequest(
+exports.scheduleBook = functions.pubsub.schedule("0 1 * * 5").onRun(
   async (
-    request: any,
+    _request: any,
     response: {
       headers: any;
       cookie: (arg0: string, arg1: any, arg3: any) => void;
@@ -18,17 +18,25 @@ exports.book = functions.https.onRequest(
       };
     }
   ) => {
-    const today = new Date();
-    const startOfNextWeek = startOfWeek(today); // start of next week
-    const tuesday = addDays(startOfNextWeek, 1); // Tuesday of next week
-    const formattedDate = format(tuesday, "yyyy-MM-dd"); // date formatée
+    const today = new Date(); // Récupère la date actuelle
+    const daysUntilNextThursday = (11 - today.getDay()) % 7; // Calcule le nombre de jours jusqu'au prochain jeudi (11 correspond au code de jour pour jeudi, modulo 7 pour éviter les résultats négatifs)
+    const nextThursday = new Date(
+      today.getTime() + daysUntilNextThursday * 86400000
+    ); // Ajoute le nombre de jours au timestamp de la date actuelle pour obtenir la date du prochain jeudi
+    const formattedDate = format(nextThursday, "yyyy-MM-dd"); // date formatée
     await postManRequest
       .post(DINARD_BOOK_API, {
         form: {
           jour: formattedDate,
-          heure: "19:00:00",
+          heure: "12:30:00",
           p: "1",
           frequence: "10",
+          /** Jean Gautier */
+          m1: "2390",
+          /** Bernard L */
+          m2: "2602",
+          /** Daniel Gautier */
+          m3: "2389",
         },
         headers: {
           Cookie: "nsgcookie=2406",
